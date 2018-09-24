80abb3edd6cdbb8d129aa698a9be60b9c51dc115
Dave Pierre <dpierre@SYS0652.local>
2018-09-24 11:12:39 -0400

    added all the finctionality expect for the css class binding

--- src/components/heroes.js
+++ src/components/heroes.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { HEROES } from '../mock-heroes';
 
+import './styles.css'
 
 
 class HeroesComponent extends Component {
@@ -7,32 +9,110 @@ class HeroesComponent extends Component {
         super(props);
         this.state ={
 
-            name: "Windstorm",
-            id :1
+            
+
+            heroes : HEROES,
+
+            selectedHero :{}
         }
 
+
+        this.onSelectHero.bind(this)
         this.handleChange.bind(this)
+        this.renderHeroList.bind(this)
+
+       
+    }
+
+    onSelectHero(heroId){
+
+        const myHeros = this.state.heroes;
+        const selectedHeroObj = (myHeros.filter(hero=> hero.id == heroId))[0]
+        console.log(selectedHeroObj)
+        this.setState({
+            selectedHero:selectedHeroObj
+            
+
+
+        })
+        
+      
+    
     }
 
     handleChange(newName) {
+
+
+        let newHero = this.state.selectedHero
+        newHero.name = newName
         this.setState({
-            name: newName
+            selectedHero : newHero
         })
 
     }
 
-  render() {
-    return (
-      <div className="App">
-      <h2>{this.state.name} Details</h2>
-      <div><span>id: </span>{this.state.id}</div>
-      <div>
-          <label>name:
-          <input type ="text" value = {this.state.name} onChange = {e => this.handleChange(e.target.value)} placeholder = "name" />
-          </label>
-      </div>
 
+
+  
+    renderHeroList(){
+        const heroesItems = (this.state.heroes).map((hero) => 
+        <li key = {hero.id.toString()} onClick= { ()=> this.onSelectHero(hero.id)}> 
+        <span className="badge">{hero.id}</span> {hero.name}
+        </li>);
+      
+    
+
+        return heroesItems;
+    
+       
+    }
+
+    renderHeroDetails(){
+
+        // The hero details from the first part
+        const selectedHeroDetail =  <div>
+            <h2>{this.state.selectedHero.name} Details</h2>
+            <div><span>id: </span>{this.state.selectedHero.id}</div>
+            <div>
+                <label>name:
+          <input type="text" value={this.state.selectedHero.name} onChange={e => this.handleChange(e.target.value)} placeholder="name" />
+                </label>
+            </div>
+
+        </div> 
+
+        // if there is a selectedHero selected then assigned it to the tobe return variabe 
+        if (this.state.selectedHero.id){
+            return selectedHeroDetail
+        }else{
+            return
+        }
         
+
+
+      
+
+    }
+
+  render() {
+
+    const heroesItems = this.renderHeroList()
+    const heroDetails = this.renderHeroDetails()
+    return (
+      <div>
+      <div >
+          <ul className= "heroes">
+              {heroesItems}
+          </ul>
+       
+      </div>
+      
+        <div>
+
+            {heroDetails}
+        </div>
+
+
       </div>
     );
   }
